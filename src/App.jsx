af9ef79c5ff9f80f9ea75a5d49257a910cc3e1ff
Naeem-Raza110 <nraza7225@gmail.com>
2026-02-02 16:34:27 +0500

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import MainLayout from "./layouts/MainLayout";
 import Home from "./pages/HomePages/Home";
 import Features from "./pages/HomePages/Features";
 import Contact from "./pages/HomePages/Contact";
-import Auth from "./pages/Auth/Auth";
+import Auth from "./pages/auth/Auth";
 import DashboardLayout from "./layouts/DashboardLayout";
 import Dashboard from "./pages/DashboardPages/Dashboard";
 import Profile from "./pages/DashboardPages/Profile";
@@ -14,25 +14,25 @@ import NotFound from "./pages/NotFound";
 
 const App = () => {
   return (
+   
+      <Routes>
+        <Route element={<MainLayout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/features" element={<Features />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/auth" element={<Auth />} />
+        </Route> 
+         <Route path="dashboard" element={<DashboardLayout />}>
+          <Route index element={<Dashboard />} />
+          <Route path="profile" element={<Profile />} />
+          <Route path="settings" element={<Settings />} />
+          <Route path="analytics" element={<Analytics />} />
+          
+        </Route>
+        <Route path="*" element={<NotFound />} />
 
-    <Routes>
-      <Route element={<MainLayout />}>
-        <Route path="/" element={<Home />} />
-        <Route path="/features" element={<Features />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/auth" element={<Auth />} />
-      </Route>
-      <Route path="dashboard" element={<DashboardLayout />}>
-        <Route index element={<Dashboard />} />
-        <Route path="profile" element={<Profile />} />
-        <Route path="settings" element={<Settings />} />
-        <Route path="analytics" element={<Analytics />} />
-
-      </Route>
-      <Route path="*" element={<NotFound />} />
-
-    </Routes>
-
+      </Routes>
+     
 
   );
 };
