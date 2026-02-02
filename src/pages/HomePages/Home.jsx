635ae9c26d362578eca98b21b48e6115735bee79
Naeem Raza <159261667+Naeem-Raza110@users.noreply.github.com>
2026-02-03 01:28:43 +0500

    fixed
    fixed

--- src/pages/HomePages/Home.jsx
+++ src/pages/HomePages/Home.jsx
@@ -1,9 +1,9 @@
 import React from "react";
-import HomeSections from "./HomeSections";
+import MainSections from "./HomeSections";
 const Home = () => {
   return (
     <div className="min-h-screen bg-[#010205] text-white relative">
-      <HomeSections />
+      <MainSections />
     </div>
   );
 };
