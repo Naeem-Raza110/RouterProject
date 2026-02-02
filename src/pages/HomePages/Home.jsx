c5cd171d81db549a08277c3832da50bdc38b87cd
Naeem-Raza110 <nraza7225@gmail.com>
2026-02-03 00:09:57 +0500

    update

--- src/pages/HomePages/Home.jsx
+++ src/pages/HomePages/Home.jsx
@@ -1,12 +1,11 @@
 import React from "react";
 import { Link } from "react-router";
 import Button from "../../components/Ui/Button";
-import HomeSections from "./HomeSections";
+import HomeSection from "./HomeSections";
 const Home = () => {
   return (
     <div className="min-h-screen bg-[#010205] text-white relative">
-    
-      <HomeSections />
+      <HomeSection />
     </div>
   );
 };
