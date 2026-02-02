ce7780f6c4a233a3584237607665826a044ff57a
Naeem-Raza110 <nraza7225@gmail.com>
2026-02-03 01:16:50 +0500

    fixed the issue

--- src/pages/HomePages/Home.jsx
+++ src/pages/HomePages/Home.jsx
@@ -1,9 +1,9 @@
 import React from "react";
-import HomeSection from "./HomeSections";
+import HomeSections from "./HomeSections";
 const Home = () => {
   return (
     <div className="min-h-screen bg-[#010205] text-white relative">
-      <HomeSection />
+      <HomeSections />
     </div>
   );
 };
