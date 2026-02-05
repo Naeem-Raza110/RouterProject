91b07219355f677a2493914e7aa6227b756a8c96
Naeem-Raza110 <nraza7225@gmail.com>
2026-02-05 16:04:14 +0500

    ADD testimonials width

--- src/pages/HomePages/HomeSections/Sections/Testimonials.jsx
+++ src/pages/HomePages/HomeSections/Sections/Testimonials.jsx
@@ -3,8 +3,9 @@ import Container from '../../Container'
 
 function Testimonials() {
   return (
-    <Container>
+    
    <section id="testimonials" className="py-24 px-6 relative z-10 bg-white/5 backdrop-blur-xl">
+    <Container>
         <h3 className="text-3xl font-semibold text-center mb-12">What Our Clients Say</h3>
         <div className="grid grid-cols-1 md:grid-cols-3 gap-10 max-w-5xl mx-auto">
           <div className="p-6 bg-gray-900/50 rounded-xl shadow-xl text-center">
@@ -20,8 +21,9 @@ function Testimonials() {
             <span className="font-semibold text-white">- Michael B.</span>
           </div>
         </div>
+          </Container>
       </section>
-        </Container>
+      
   )
 }
 
